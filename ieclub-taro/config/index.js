f9e553dcc604ef8aa1088a02e15f5adfe79b73f9
xingduoweiyun <12310203@mail.sustech.edu.cn>
2025-10-26 19:22:12 +0800

    up

---
+++ ieclub-taro/config/index.js
@@ -0,0 +1,30 @@
+const config = {
+  projectName: 'ieclub-taro',
+  date: '2023-10-26',
+  designWidth: 750,
+  deviceRatio: {
+    640: 2.34 / 2,
+    750: 1,
+    828: 1.81 / 2
+  },
+  sourceRoot: 'src',
+  outputRoot: 'dist',
+  plugins: [
+    '@tarojs/plugin-platform-h5'
+  ],
+  defineConstants: {
+  },
+  copy: {
+    patterns: [
+    ],
+    options: {
+    }
+  },
+  framework: 'react',
+  compiler: 'webpack5',
+  cache: {
+    enable: false
+  }
+}
+
+module.exports = config
